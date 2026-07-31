0529ec2b5e36c6ec5a3dbbe83039665f3df409e5
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2026-07-31 11:56:58 +0200

    Adds vite config and main.js file

---
+++ assets/src/js/main.js
@@ -0,0 +1,14 @@
+// src/scripts/main.js
+
+/**
+ * Main JS file for Cartify Child
+ * 
+ * Import the main css file
+ * Import all the JavaScript files that are needed for the theme.
+ */
+
+// Import the main CSS file
+import '../scss/styles.scss'
+
+// Build the main JS file
+// ... other js files imports
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,19 @@
 //vite.config.mjs
 import { defineConfig } from 'vite';
 
+const JS_FILE = resolve('assets/src/scripts/main.js')
+
+// Define where the compiled and minified JavaScript files will be saved
+const BUILD_DIR = resolve(__dirname, 'dist');
+
 export default defineConfig({
-  // Vite configuration options
+  build: {
+    assetsDir: '', // Will save the compiled JavaScript files in the root of the dist folder
+    manifest: true, // Generate manifest.json file (for caching)
+    emptyOutDir: true, // Empty the dist folder before building
+    outDir: BUILD_DIR,
+    rollupOptions: {
+      input: JS_FILE,
+    },
+  },
 });
