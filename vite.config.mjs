0e33e3a22444ccc96aae426e6046987afc3f61d3
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2026-07-31 13:54:21 +0200

    Upgrades the vite conf and adds the correct JavaScript location

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 //vite.config.mjs
 import { defineConfig } from 'vite';
 
-const JS_FILE = resolve('assets/src/scripts/main.js')
+const JS_FILE = resolve('assets/src/js/main.js')
 
 // Define where the compiled and minified JavaScript files will be saved
 const BUILD_DIR = resolve(__dirname, 'dist');
