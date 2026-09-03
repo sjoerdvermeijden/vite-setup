b55c234df8e333b82364b9c4643de4af1b693d3c
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2026-09-03 15:26:03 +0200

    Makes Browsersyc work

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import autoprefixer from 'autoprefixer'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import VitePluginBrowserSync from 'vite-plugin-browser-sync'
 
 const JS_FILE = path.resolve('assets/src/js/main.js')
@@ -11,17 +12,38 @@ const JS_FILE = path.resolve('assets/src/js/main.js')
 const BUILD_DIR = path.resolve(__dirname, 'assets/dist');
 
 export default defineConfig({
-  plugins: [VitePluginBrowserSync({
+  plugins: [
+    VitePluginBrowserSync({
       dev: {
         bs: {
+          proxy: 'http://icons.local',
           ui: {
             port: 8080
           },
-          notify: false,
-          files: ['./assets/src/**/*'],
+          notify: false
+        }
+      },
+      buildWatch: {
+        enable: true,
+        bs: {
+          proxy: 'http://icons.local',
+          files: ['assets/dist/**/*'],
+          ui: {
+            port: 8080
+          },
+          notify: false
         }
       }
-  })],
+    }),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), './assets/src/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+      // custom dom id
+      customDomId: '__svg__icons__dom__',
+    }),
+  ],
   css: {
     postcss: {
       plugins: [
